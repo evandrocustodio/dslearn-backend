74d05661fcf399805616cf7d8f12d66672863257
Evandro Custodio <evandrocustodio@gmail.com>
2023-01-20 18:10:56 -0300

    Estilização da tela HOME

--- frontend/frontweb/src/pages/Home/indexe.tsx
+++ frontend/frontweb/src/pages/Home/indexe.tsx
@@ -1,26 +1,28 @@
-import { ReactComponent as MainImage } from "assets/images/main-image.svg";
-import ButtonIcon from "components/ButtonIcon";
-import NavBar from "components/Navbar";
+import { ReactComponent as MainImage } from 'assets/images/main-image.svg';
+import ButtonIcon from 'components/ButtonIcon';
+import NavBar from 'components/Navbar';
 import './style.css';
 const Home = () => {
   return (
-    <div >
-          <NavBar />
-          <div className="home-container">
-             <div className="home-card">
-                  <div className="home-content-container">
-                    <h1>Conheça o melhor catálogo de produtos</h1>
-                    <p>Ajudaremos você a encontrar os melhores produtos disponíveis no mercado.</p>
-                    <ButtonIcon />
-                  </div>
-                  <div className="home-image-container">
-                    <MainImage />
-                  </div>
-
-             </div>
-
+    <div>
+      <NavBar />
+      <div className="home-container">
+        <div className="home-card">
+          <div className="home-content-container">
+            <div>
+              <h1>Conheça o melhor catálogo de produtos</h1>
+              <p>
+                Ajudaremos você a encontrar os melhores produtos disponíveis no
+                mercado.
+              </p>
+            </div>
+            <ButtonIcon />
           </div>
-
+          <div className="home-image-container">
+            <MainImage />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
